c3aa9a4ec7943cb8328db1fa2ee838336c1791e6
apollycn <apollycn@gmail.com>
2024-11-06 19:41:57 -0500

    feat: confetti should show when clicking menu buttons

---
+++ game/src/js/menu.js
@@ -0,0 +1,25 @@
+const buttons = document.getElementsByClassName("container");
+
+const onClick = (event) => {
+	const button = event.target;
+	const rect = button.getBoundingClientRect();
+
+	const buttonX = rect.left + rect.width / 2;
+	const buttonY = rect.top + rect.height / 2;
+
+	// eslint-disable-next-line no-undef
+	confetti({
+		particleCount: 75,
+		spread: 180,
+		startVelocity: 15,
+		origin: {
+			x: buttonX / window.innerWidth,
+			y: buttonY / window.innerHeight,
+		},
+		colors: ["#ff6f61", "#ff9f91", "#ff4a36"],
+	});
+};
+
+[...buttons].forEach((button) => {
+	button.addEventListener("click", onClick);
+});
