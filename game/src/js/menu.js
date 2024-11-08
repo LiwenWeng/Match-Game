5ebc4c6f9d2cf49fa6fb698fb57b7ff3557d3cb0
apollycn <apollycn@gmail.com>
2024-11-07 20:04:42 -0500

    feat: popup animations

--- game/src/js/menu.js
+++ game/src/js/menu.js
@@ -1,25 +1,14 @@
-const buttons = document.getElementsByClassName("container")[0].children[1].children;
+const helpButton = document.getElementById("help");
 
-const onClick = (event) => {
-	const button = event.target;
-	const rect = button.getBoundingClientRect();
+const helpPopup = document.getElementById("help-popup");
+const confirmPopupButton = document.getElementById("confirm");
 
-	const buttonX = rect.left + rect.width / 2;
-	const buttonY = rect.top + rect.height / 2;
-
-	// eslint-disable-next-line no-undef
-	confetti({
-		particleCount: 75,
-		spread: 180,
-		startVelocity: 15,
-		origin: {
-			x: buttonX / window.innerWidth,
-			y: buttonY / window.innerHeight,
-		},
-		colors: ["#ff6f61", "#ff9f91", "#ff4a36"],
-	});
+helpButton.onclick = () => {
+	helpPopup.classList.remove("close");
+	helpPopup.classList.add("open");
 };
 
-[...buttons].forEach((button) => {
-	button.addEventListener("click", onClick);
-});
+confirmPopupButton.onclick = () => {
+	helpPopup.classList.remove("open");
+	helpPopup.classList.add("close");
+};
