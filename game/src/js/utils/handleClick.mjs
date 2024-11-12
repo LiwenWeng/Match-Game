1a0857969e878c80bbfe15e3babb7a05d438e6c8
apollycn <apollycn@gmail.com>
2024-11-12 02:08:48 -0500

    fix: fix unable to continue game after a match

--- game/src/js/utils/handleClick.mjs
+++ game/src/js/utils/handleClick.mjs
@@ -24,6 +24,10 @@ export const handleClick = (card, clickState) => {
 document.addEventListener("clickState", (e) => {
 	if (e.detail.clickState === 2) {
 		canClick = false;
+		setTimeout(() => {
+			canClick = true;
+		}, 2000);
+
 		const card1 = e.detail.card1;
 		const card2 = e.detail.card2;
 
@@ -33,7 +37,6 @@ document.addEventListener("clickState", (e) => {
 				card2.classList.toggle("flipped");
 				card1.setAttribute("flipped", false);
 				card2.setAttribute("flipped", false);
-				canClick = true;
 			}, 2000);
 		} else {
 			matchedAmount++;
